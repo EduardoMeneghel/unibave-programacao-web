5cec389ea6be51ed3b54432e8e677c840aee8e44
Eduardo Meneghel <eduardopedromeneghel@outlook.com.br>
2023-03-22 23:27:39 -0300

    feat/adicionando readme.md e adicionando frase em localStorage

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ function novaFrase() {
     fetch('https://api.kanye.rest/')
       .then(response => response.text())
       .then(textJson => {
-        document.getElementById('fetch').textContent = JSON.parse(textJson).quote;
+        document.getElementById('fetch').innerHTML = JSON.parse(textJson).quote;
+        localStorage.setItem("frase", JSON.parse(textJson).quote);
       });
   }
-  cache();
+cache();
 function cache(){
     if (localStorage.getItem("nome") != null) {
         nome.innerHTML =  localStorage.getItem("nome");
@@ -15,35 +16,38 @@ function cache(){
         genero.innerHTML = localStorage.getItem("genero");
         episodio.href = localStorage.getItem("episodio");
     }
+    if (localStorage.getItem("frase") != null) {
+      document.getElementById('fetch').innerHTML = localStorage.getItem("frase");
+    }
 }
 
-  async function novoPersonagem() {
+async function novoPersonagem() {
 
-    try {
-        const response = await fetch('https://rickandmortyapi.com/api');
+  try {
+      const response = await fetch('https://rickandmortyapi.com/api');
 
-        const data = await response.json();
-        const page = Math.floor(Math.random() * 42);
-        const charactersJson = await fetch(data.characters+'/?page='+page);
-        const characters = await charactersJson.json();
-        const numero = Math.floor(Math.random() * characters.results.length);
-        const character = characters.results[numero];
-        nome.innerHTML = character.name;
-        console.log(character);
-        imagem.src = character.image;
-        especie.innerHTML = character.species;
-        tipo.innerHTML = character.type;
-        genero.innerHTML = character.gender;
-        episodio.href = character.episode[0];
-        localStorage.setItem("nome", character.name);
-        localStorage.setItem("especie", character.species);
-        localStorage.setItem("imagem", character.image);
-        localStorage.setItem("tipo", character.type);
-        localStorage.setItem("genero", character.gender);
-        localStorage.setItem("episodio", character.episode[0]);
-    } catch (error) {
-      console.error(error);
-    }
+      const data = await response.json();
+      const page = Math.floor(Math.random() * 42);
+      const charactersJson = await fetch(data.characters+'/?page='+page);
+      const characters = await charactersJson.json();
+      const numero = Math.floor(Math.random() * characters.results.length);
+      const character = characters.results[numero];
+      nome.innerHTML = character.name;
+      console.log(character);
+      imagem.src = character.image;
+      especie.innerHTML = character.species;
+      tipo.innerHTML = character.type;
+      genero.innerHTML = character.gender;
+      episodio.href = character.episode[0];
+      localStorage.setItem("nome", character.name);
+      localStorage.setItem("especie", character.species);
+      localStorage.setItem("imagem", character.image);
+      localStorage.setItem("tipo", character.type);
+      localStorage.setItem("genero", character.gender);
+      localStorage.setItem("episodio", character.episode[0]);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function removeCache() {
@@ -53,4 +57,5 @@ function removeCache() {
     localStorage.removeItem("tipo");
     localStorage.removeItem("genero");
     localStorage.removeItem("episodio");
+    localStorage.removeItem("frase");
 }
